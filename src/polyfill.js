1bb2ac11befdad09f5d57aeff5f302ea0f1f16c2
Hanterdro <Hanterdro@users.noreply.github.com>
2017-10-30 18:05:37 +0100

    Fixed a bug which prevents that a delegation on the form will not trigger events of child inputs (#4)
    * jsdoc: use correct optional syntax

---
+++ src/polyfill.js
@@ -0,0 +1,16 @@
+// from https://developer.mozilla.org/de/docs/Web/API/Element/closest
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    let el = this;
+    if (!document.documentElement.contains(el)) return null;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement;
+    } while (el !== null);
+    return null;
+  };
+}
